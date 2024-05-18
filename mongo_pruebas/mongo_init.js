78b7bd8f3be5bc93eece8ba7c74711978392a71b
Gespinosa <gespinosa01@icloud.com>
2024-05-18 15:40:25 -0700

    Mongo01 como primario

--- mongo_pruebas/mongo_init.js
+++ mongo_pruebas/mongo_init.js
@@ -1,10 +1,10 @@
 rs.initiate({
   _id: "rs0",
   members: [
-    { _id: 0, host: "mongo01:27017" },
-    { _id: 1, host: "mongo02:27017" },
-    { _id: 2, host: "mongo03:27017" },
-    { _id: 3, host: "mongo04:27017" }
+    { _id: 0, host: "mongo01:27017", priority: 2 },
+    { _id: 1, host: "mongo02:27017", priority: 1 },
+    { _id: 2, host: "mongo03:27017", priority: 1 },
+    { _id: 3, host: "mongo04:27017", priority: 1 }
   ]
 });
 
