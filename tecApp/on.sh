1da4e77a74dc14d0da3b06226c718674cac50450
Gespinosa <gespinosa01@icloud.com>
2024-05-19 17:28:50 -0700

    Construir solo contenedor tecapp

--- tecApp/on.sh
+++ tecApp/on.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Activar docker-compose
-docker-compose up -d --build
+docker-compose up -d --no-deps --build app
 
 # Esperar a que se active
 sleep 5
 
 # Copiar el script de replica set
-xclip -selection clipboard < mongo_init.js
+#xclip -selection clipboard < mongo_init.js
 
 # Ejecutar contenedor de mongo para pegar script de replica
-docker exec -it mongo01 bash -c mongosh
+#docker exec -it mongo01 bash -c mongosh
