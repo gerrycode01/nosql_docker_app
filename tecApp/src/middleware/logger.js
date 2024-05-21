beffa36119f360108a70165da0ce6af21223b902
Gespinosa <gespinosa01@icloud.com>
2024-05-21 00:20:49 -0700

    Intentando redis

--- tecApp/src/middleware/logger.js
+++ tecApp/src/middleware/logger.js
@@ -1,37 +1,62 @@
-const { redisClient } = require('../config/db');
+// /src/middleware/logger.js
+const redis = require("redis");
+const client = redis.createClient({
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+});
+
+client.on("error", (err) => {
+  console.error("Redis error de conexion:", err);
+});
+
+client.connect()
+  .then(() => {
+    console.log("Conectado-->> Redis");
+  })
+  .catch((err) => {
+    console.error("Error conexion a Redis:", err);
+  });
 
 module.exports = (req, res, next) => {
-  res.on('finish', () => {
-    console.log(`Log: Processing ${req.method} ${req.originalUrl}`);  // Log de diagnóstico
+  let originalWrite = res.write;
+  let originalEnd = res.end;
+  let chunks = [];
 
-    if (!redisClient.isOpen) {
-      console.error('Redis client -->> No conectado.');
-      return;
+  res.write = function (chunk, ...args) {
+    chunks.push(chunk);
+    originalWrite.apply(res, [chunk, ...args]);
+  };
+
+  res.end = function (chunk, ...args) {
+    if (chunk) {
+      chunks.push(chunk);
+    }
+    const body = Buffer.concat(chunks).toString('utf8');
+    res.body = body;
+
+    if (!client.isOpen) {
+      console.error("Redis client -->> No conectado.");
+    } else {
+      const key = `${req.method}:${Date.now()}:${req.originalUrl}`;
+      const logEntry = JSON.stringify({
+        req: {
+          time: new Date(),
+          method: req.method,
+          url: req.originalUrl,
+          body: req.body,
+        },
+        res: {
+          method: res.method,
+          body: body,
+        },
+      });
+      client.set(key, logEntry, "EX", 60 * 60 * 24)
+        .catch((err) => {
+          console.error("Error al guardar:", err);
+        });
     }
 
-    const key = `${req.method}:${Date.now()}:${req.originalUrl}`;
-    const logEntry = JSON.stringify({
-      time: new Date(),
-      req: {
-        method: req.method,
-        url: req.originalUrl,
-        headers: req.headers,
-        body: req.body
-      },
-      res: {
-        statusCode: res.statusCode,
-        statusMessage: res.statusMessage
-      }
-    });
-
-    redisClient.set(key, logEntry, 'EX', 60 * 60 * 24, (err) => {
-      if (err) {
-        console.error('Error al salvar en Redis:', err);
-      } else {
-        console.log(`Log saved: ${key}`);  // Confirmación de que el log fue guardado
-      }
-    });
-  });
+    originalEnd.apply(res, [chunk, ...args]);
+  };
 
   next();
 };
